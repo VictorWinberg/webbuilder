99cc95caf7f44bd8109a651ecbed34dd75091b67
Victor Winberg <victor.m.winberg@gmail.com>
2019-11-10 16:56:26 +0100

    Refactor client comment

--- boilerplate/client/src/app/comments/comment-list.vue
+++ boilerplate/client/src/app/comments/comment-list.vue
@@ -1,103 +1,22 @@
 <template>
   <div>
-    <span>PostId</span>
-    <input type="text" v-model="postId" />
-    <br />
-    <textarea v-model="comment" />
-    <br />
-    <button v-on:click="addComment()">CREATE</button>
-    <ul>
-      <li v-for="comment in comments" v-bind:key="comment.id">
-        <hr />
-        <b>{{ comment.comment }}</b>
-        <br />
-        PostId: {{ comment.postId }}
-        <br />
-        <button v-on:click="showComment(comment.id)">SHOW</button>
-        <button v-on:click="editComment(comment.id)">EDIT</button>
-        <button v-on:click="removeComment(comment.id)">REMOVE</button>
-      </li>
-      <hr />
-    </ul>
+    <comment-new></comment-new>
+    <comment-list-table></comment-list-table>
   </div>
 </template>
 
 <script>
+import CommentNew from "./comment-new";
+import CommentListTable from "./comment-list-table";
+
 export default {
   name: "comment-list",
-  data() {
-    return {
-      postId: "",
-      comment: "",
-      comments: [],
-      loading: false
-    };
-  },
-  created() {
-    this.refreshComments();
-  },
-  methods: {
-    async refreshComments() {
-      this.loading = true;
-      const comments = await fetch("/api/comments");
-      this.comments = await comments.json();
-      this.loading = false;
-    },
-    async addComment() {
-      if (this.valid()) {
-        const res = await fetch("/api/comments", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            postId: this.postId,
-            comment: this.comment
-          })
-        });
-
-        if (res.err) {
-          console.error(res.err);
-          return;
-        }
-
-        this.postId = "";
-        this.comment = "";
-        await this.refreshComments();
-      }
-    },
-    showComment(id) {
-      this.$router.push({
-        name: "comment-show",
-        params: { id }
-      });
-    },
-    editComment(id) {
-      this.$router.push({
-        name: "comment-edit",
-        params: { id }
-      });
-    },
-    async removeComment(id) {
-      const res = await fetch(`/api/comments/${id}`, {
-        method: "DELETE"
-      });
-      if (res.err) {
-        console.error(res.err);
-        return;
-      }
-      await this.refreshComments();
-    },
-    valid() {
-      return this.title !== "" && this.content !== "";
-    }
+  components: {
+    CommentNew,
+    CommentListTable
   }
 };
 </script>
 
 <style scoped>
-ul {
-  list-style: none;
-  padding: 0;
-}
 </style>
