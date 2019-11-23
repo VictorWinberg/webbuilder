1ee0fb22ce2ec4cfa45952fdac94bd1b0b96a668
Victor Winberg <victor.m.winberg@gmail.com>
2019-11-23 12:26:27 +0100

    Add entities.json routes

---
+++ server/routes/entities.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const util = require("util");
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const ENTITIES_PATH = "../entities.json";
+
+module.exports = (app, db) => {
+  app.get("/api/entities", async (req, res) => {
+    try {
+      const entities = await readFile(ENTITIES_PATH, "utf8");
+      return res.send(JSON.parse(entities));
+    } catch (err) {
+      console.log("Error reading entities.json", JSON.stringify(err));
+      return res.send(err);
+    }
+  });
+
+  app.post("/api/entities", async (req, res) => {
+    try {
+      await writeFile(ENTITIES_PATH, JSON.stringify(req.body), "utf8");
+      const entities = await readFile(ENTITIES_PATH, "utf8");
+      return res.send(JSON.parse(entities));
+    } catch (err) {
+      console.log("Error creating entities.json", JSON.stringify(err));
+      return res.status(400).send(err);
+    }
+  });
+};
