f7bd91da98e084fa4a7c2cfd110239d37c6e9172
unknown <victor.m.winberg@gmail.com>
2019-11-29 18:31:10 +0100

    Optimize build

--- server/compiler/build.js
+++ server/compiler/build.js
@@ -5,38 +5,23 @@ const app = name => `${APP_PATH}/${name}`;
 const template = name => `./templates/${name}`;
 
 const build = async entities => {
-  // ENTITIES ROUTE
-  const ENTITIES_PATH = APP_PATH + "/entities";
-  if (!exists(ENTITIES_PATH)) {
-    await mkdir(ENTITIES_PATH);
-  }
-
-  const template = await readFile("./templates/entities/entities.vue");
-  const contents = format(template, "entities", { entities: JSON.stringify(entities) });
-  await writeFile(ENTITIES_PATH + "/entities.vue", contents);
-
-  const entitiesRoutes = await readFile(
-    "./templates/entities/entities.routes.js"
+  // TODO: Add more templates and "template-choosing"
+  const TEMPLATE_FOLDER = `./templates/basic`;
+  const templates = await Promise.all(
+    readdir(TEMPLATE_FOLDER)
+      .map(async component => ({ component, file: await readFile(`${TEMPLATE_FOLDER}/${component}`) }))
   );
-  await writeFile(ENTITIES_PATH + "/entities.routes.js", entitiesRoutes);
 
-  // BUILD ENTITIES
   entities.forEach(async ({ name, fields }) => {
-    const TEMPLATE_FOLDER = `./templates/entity`;
-
     const ENTITY_FOLDER = app(name);
     if (!exists(ENTITY_FOLDER)) {
       await mkdir(ENTITY_FOLDER);
     }
 
-    // TODO: Optimization: Templates should be read once into a map that then can be used
-    const templates = readdir(TEMPLATE_FOLDER);
+    templates.forEach(async ({ component, file }) => {
+      const contents = format(file, name);
 
-    templates.forEach(async template => {
-      const temp = await readFile(`${TEMPLATE_FOLDER}/${template}`);
-      const contents = format(temp, name);
-
-      await writeFile(`${ENTITY_FOLDER}/${name}-${template}`, contents);
+      await writeFile(`${ENTITY_FOLDER}/${name}-${component}`, contents);
     });
   });
 };
