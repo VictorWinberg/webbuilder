9c7f79fac4fa812d1fb576db4d5098b3945cd3fe
Victor Winberg <victor.m.winberg@gmail.com>
2019-12-22 22:26:49 +0100

    Fix test

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,47 +1,17 @@
 <template>
   <div class="home">
-    <h1>Webbuilder™</h1>
-    <h3>Routes</h3>
-    <ul>
-      <li v-for="route in routes" v-bind:key="route">
-        <router-link :to="route">{{route}}</router-link>
-      </li>
-    </ul>
+    <h1>Webbuilder</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home",
-  data() {
-    return {};
-  },
-  computed: {
-    routes() {
-      const req = require.context("../app", true, /routes\.js$/);
-      return req
-        .keys()
-        .flatMap(filename => req(filename).default)
-        .map(c => c.path);
-    }
-  }
+  name: "home"
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
--- client/tests/unit/example.spec.ts
+++ client/tests/unit/example.spec.ts
@@ -1,12 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Home from "@/components/Home.vue";
 
-describe("HelloWorld.vue", () => {
+describe("Home.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
+    const wrapper = shallowMount(Home);
+    expect(wrapper.text()).toMatch("Webbuilder");
   });
 });
