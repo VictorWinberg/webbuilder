f8a6d07b33393d839f53a1277006306cc11bfa1d
Victor Winberg <victor.m.winberg@gmail.com>
2019-12-27 12:54:54 +0100

    Update eslint

--- client/tests/unit/example.spec.ts
+++ client/tests/unit/example.spec.ts
@@ -2,7 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import Home from "@/components/Home.vue";
 
 describe("Home.vue", () => {
-  it("renders", () => {
+  it("renders", (): void => {
     const wrapper = shallowMount(Home);
     expect(wrapper.text()).toMatch("Webbuilder");
   });
