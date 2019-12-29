0a9af1f0ce0d786ddc9daee252c7238d661863f6
Victor Winberg <victor.m.winberg@gmail.com>
2019-12-29 11:26:00 +0100

    Add with plugin

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,15 +1,12 @@
 import Vue from "vue";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
 
+import "./plugins/element.js";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
-Vue.use(ElementUI);
 
 new Vue({
   router,
