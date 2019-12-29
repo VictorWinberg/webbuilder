f146ac2d1d126f145bb8053cb8b4dbfce9fb2c62
Victor Winberg <victor.m.winberg@gmail.com>
2019-12-29 11:20:00 +0100

    Add element ui

--- client/src/components/entity/entity-new.vue
+++ client/src/components/entity/entity-new.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
-        <span>Title</span>
-        <input type="text" v-model="title" />
-        <br />
-        <textarea v-model="content" />
-        <br />
-        <button v-on:click="addEntity()">CREATE</button>
+        <el-aside>Hello</el-aside>
+        <el-container>
+            <div>
+                <span>Title</span>
+                <input type="text" v-model="title" />
+                <el-input placeholder="Pick a date" suffix-icon="el-icon-edit" v-model="title"> </el-input>
+                <br />
+                <textarea v-model="content" />
+                <br />
+                <button v-on:click="addPost()">CREATE</button>
+            </div>
+            <el-button type="success" v-on:click="addPost()">CREATE</el-button>
+        </el-container>
     </div>
 </template>
 
