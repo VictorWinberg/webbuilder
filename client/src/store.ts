da9185f36c04643246dc4b5d6868857d5d76859e
Victor Winberg <victor.m.winberg@gmail.com>
2019-12-29 23:52:10 +0100

    Fix typescript warnings

--- client/src/store.ts
+++ client/src/store.ts
@@ -5,7 +5,7 @@ import { mergeAll } from "lodash/fp";
 Vue.use(Vuex);
 
 const req = require.context("./app", true, /store\.ts$/);
-const stores = req.keys().flatMap(filename => req(filename).default);
+const stores = req.keys().flatMap((filename): {} => req(filename).default);
 
 export default new Vuex.Store({
   state: {},
