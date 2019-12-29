d65c3240f7a8aa7002ceca93fe8b92597695dad8
Annie Onnered <annie.onnered@gmail.com>
2019-12-29 11:01:35 +0100

    Start on entity form

--- client/src/components/entity/entity-edit.vue
+++ client/src/components/entity/entity-edit.vue
@@ -1,67 +1,118 @@
 <template>
-  <div>
-    <span>Title</span>
-    <input type="text" v-model="entity.title" />
-    <br />
-    <textarea v-model="entity.content" />
-    <br />
-    <button v-on:click="back()">GO BACK</button>
-    <button v-on:click="showEntity(id)">SHOW</button>
-    <button v-on:click="editEntity(id)">SAVE</button>
-  </div>
+    <div>
+        <span>Hello {{ name }}</span>
+        <br />
+        <v-data-table
+            :headers="headers"
+            :items="items"
+            hide-default-footer
+            item-key="name"
+            class="elevation-1"
+        >
+            <template v-slot:item.name="{ item }">
+                <div>
+                    <v-text-field required v-model="item.name" />
+                </div>
+            </template>
+            <template v-slot:item.relation="{ item }">
+                <v-row>
+                    <v-select :items="[item.name]" v-model="item.name" />
+                </v-row>
+                <!-- <v-row /> -->
+            </template>
+            <template v-slot:item.validations="{ item }">
+                <div
+                    v-for="(val, index) in item.validations"
+                    v-bind:key="index"
+                >
+                    <div>
+                        <v-select
+                            label="key"
+                            :items="[item.validations[index].key]"
+                            v-model="item.validations[index].key"
+                        />
+                    </div>
+                    <div>
+                        <v-select
+                            label="value"
+                            :items="[item.validations[index].value]"
+                            v-model="item.validations[index].value"
+                        />
+                    </div>
+                </div>
+                <!-- <v-row /> -->
+            </template>
+            <template v-slot:item.type="{ item }">
+                <v-row>
+                    <v-select :items="[item.type]" v-model="item.type" />
+                </v-row>
+                <!-- <v-row /> -->
+            </template>
+        </v-data-table>
+    </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: "entity-edit",
-  props: {
-    id: { required: true }
-  },
-  data() {
-    return {
-      loading: false
-    };
-  },
-  computed: {
-    entity() {
-      return this.$store.state.entity.current;
-    }
-  },
-  methods: {
-    async refreshEntity() {
-      this.loading = true;
-      await this.$store.dispatch("entity/read", this.id);
-      this.loading = false;
+    name: 'entity-edit',
+    props: {
+        name: { required: true }
     },
-    async editEntity(id: string) {
-      if (this.valid()) {
-        await this.$store.dispatch("entity/update", {
-          id,
-          title: this.entity.title,
-          content: this.entity.content
-        });
+    data() {
+        return {
+            loading: false
+        };
+    },
+    computed: {
+        entity() {
+            return this.$store.state.entity.current;
+        },
+        headers() {
+            const keys = Object.keys(this.entity.fields[0]);
+            return keys.map(obj => ({
+                text: obj,
+                value: obj
+            }));
+        },
+        items() {
+            return this.entity.fields;
+        }
+    },
+    methods: {
+        async refreshEntity() {
+            this.loading = true;
+            await this.$store.dispatch('entity/read', this.name);
+            this.loading = false;
+        },
+        async editEntity(id: string) {
+            if (this.valid()) {
+                await this.$store.dispatch('entity/update', {
+                    name,
+                    title: this.entity.title,
+                    content: this.entity.content
+                });
 
-        this.back();
-      }
+                this.back();
+            }
+        },
+        valid(): boolean {
+            return this.entity.title !== '' && this.entity.content !== '';
+        },
+        showEntity(id: string) {
+            this.$router.push({
+                name: 'entity-show',
+                params: { id }
+            });
+        },
+        back() {
+            this.$router.push({ name: 'entity-list' });
+        }
     },
-    valid(): boolean {
-      return this.entity.title !== "" && this.entity.content !== "";
-    },
-    showEntity(id: string) {
-      this.$router.push({
-        name: "entity-show",
-        params: { id }
-      });
-    },
-    back() {
-      this.$router.push({ name: "entity-list" });
+    created() {
+        this.refreshEntity();
     }
-  },
-  created() {
-    this.refreshEntity();
-  }
 });
 </script>
 
--- client/src/components/entity/entity-store.ts
+++ client/src/components/entity/entity-store.ts
@@ -1,70 +1,66 @@
 export default {
-  entity: {
-    namespaced: true,
-    state: {
-      current: {},
-      all: []
-    },
-    mutations: {
-      setEntity: (state: any, current: any) => {
-        state.current = current;
-      },
-      setEntities: (state: any, all: []) => {
-        state.all = all;
-      }
-    },
-    actions: {
-      async create(_: any, payload: any) {
-        const res = await fetch("/api/entities>", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(payload)
-        });
-
-        if (!res.ok) {
-          throw new Error(res.statusText);
+    entity: {
+        namespaced: true,
+        state: {
+            current: {},
+            all: []
+        },
+        mutations: {
+            setEntity: (state: any, current: any) => {
+                state.current = current;
+            },
+            setEntities: (state: any, all: []) => {
+                state.all = all;
+            }
+        },
+        actions: {
+            async create(_: any, payload: any) {
+                // const res = await fetch('/api/entities', {
+                //     method: 'POST',
+                //     headers: {
+                //         'Content-Type': 'application/json'
+                //     },
+                //     body: JSON.stringify(payload)
+                // });
+                // if (!res.ok) {
+                //     throw new Error(res.statusText);
+                // }
+            },
+            async list({ commit }: any) {
+                const res = await fetch('/api/entities');
+                if (!res.ok) {
+                    throw new Error(res.statusText);
+                }
+                commit('setEntities', await res.json());
+            },
+            async read({ commit }: any, id: string) {
+                // const res = await fetch(`/api/entities/${id}`);
+                // if (!res.ok) {
+                //     throw new Error(res.statusText);
+                // }
+                // commit('setEntity', await res.json());
+            },
+            async update({ commit }: any, { id = null, ...payload }) {
+                // const res = await fetch(`/api/entities/${id}`, {
+                //     method: 'PUT',
+                //     headers: {
+                //         'Content-Type': 'application/json'
+                //     },
+                //     body: JSON.stringify(payload)
+                // });
+                // if (!res.ok) {
+                //     throw new Error(res.statusText);
+                // }
+                // commit('setEntity', await res.json());
+            },
+            async remove({ commit }: any, id: string) {
+                // const res = await fetch(`/api/entities/${id}`, {
+                //     method: 'DELETE'
+                // });
+                // if (!res.ok) {
+                //     throw new Error(res.statusText);
+                // }
+            }
         }
-      },
-      async list({ commit }: any) {
-        const res = await fetch("/api/entities");
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        commit("setEntities", await res.json());
-      },
-      async read({ commit }: any, id: string) {
-        const res = await fetch(`/api/entities/${id}`);
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        commit("setEntity", await res.json());
-      },
-      async update({ commit }: any, { id = null, ...payload }) {
-        const res = await fetch(`/api/entities/${id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(payload)
-        });
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        commit("setEntity", await res.json());
-      },
-      async remove({ commit }: any, id: string) {
-        const res = await fetch(`/api/entities/${id}`, {
-          method: "DELETE"
-        });
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-      }
     }
-  }
 };
-
-
-
