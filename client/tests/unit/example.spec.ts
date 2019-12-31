42abb5741764f7fa5225db661d34683c2fd98df1
Victor Winberg <victor.m.winberg@gmail.com>
2019-12-31 10:57:57 +0100

    Fix test

--- client/tests/unit/example.spec.ts
+++ client/tests/unit/example.spec.ts
@@ -4,6 +4,6 @@ import Home from "@/components/Home.vue";
 describe("Home.vue", (): void => {
   it("renders", (): void => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.text()).toMatch("Webbuilder");
+    expect(wrapper.find("h3").text()).toMatch("Welcome to Your Webpage");
   });
 });
