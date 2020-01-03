69c223df6b69780058fdcbd88bee42cfabf8b354
Victor Winberg <victor.m.winberg@gmail.com>
2020-01-03 10:18:29 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -15,5 +15,5 @@ db.sequelize.sync({ alter: true });
 
 app.listen(process.env.PORT || 3000, () => {
   // tslint:disable-next-line:no-console
-  console.log("Server is up on port " + process.env.PORT || 3000);
+  console.log("Server is up on port " + (process.env.PORT || 3000));
 });
