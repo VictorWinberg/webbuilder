edef11d5044590589bb9522025082066275f297a
AnnieLeonia <annie.onnered@gmail.com>
2020-01-09 20:03:55 +0100

    Hotfix/v0.1 alpha (#119)
    * Add static and dynamic routes
    
    * Add responsive navigation menu
    
    * Bugfix, menu appears upon start
    
    * Show entity name in list table
    
    * Bugfix, navbar appears on top on content

--- client/src/router.ts
+++ client/src/router.ts
@@ -14,18 +14,10 @@ const defaultRoutes: RouteConfig[] = [
   {
     path: "/",
     name: "Home",
-    meta: "Home",
+    meta: { name: "Home", static: true, icon: "home" },
     component: Home
   },
   ...EntityRoutes,
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
   {
     path: "*",
     component: NotFound
