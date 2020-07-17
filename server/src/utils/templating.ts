3516005c2fbd30b72ec5c75236fcc40ed6040f50
Victor Winberg <victor.m.winberg@gmail.com>
2020-07-17 10:32:50 +0200

    Template capitilize to upperfirst (#167)

--- server/src/utils/templating.ts
+++ server/src/utils/templating.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import { capitalize, toUpper, transform } from "lodash";
+import { upperFirst, toUpper, transform } from "lodash";
 import Handlebars from "handlebars";
 import helpers from "handlebars-helpers";
 import { format } from "prettier";
@@ -7,20 +7,20 @@ import pluralize from "pluralize";
 
 Handlebars.registerHelper(helpers());
 
-Handlebars.registerHelper("switch", function(this: any, value, options) {
+Handlebars.registerHelper("switch", function (this: any, value, options) {
   this.switch_value = value;
   this.switch_break = false;
   return options.fn(this);
 });
 
-Handlebars.registerHelper("case", function(this: any, value, options) {
+Handlebars.registerHelper("case", function (this: any, value, options) {
   if (value == this.switch_value) {
     this.switch_break = true;
     return options.fn(this);
   }
 });
 
-Handlebars.registerHelper("otherwise", function(this: any, _, options) {
+Handlebars.registerHelper("otherwise", function (this: any, _, options) {
   if (this.switch_break == false) {
     return options.fn(this);
   }
@@ -32,10 +32,10 @@ function mustachify(obj: any): any {
       result[key] = mustachify(value);
     } else if (typeof value === "string") {
       result[key] = value;
-      result[capitalize(key)] = capitalize(value);
+      result[upperFirst(key)] = upperFirst(value);
       result[toUpper(key)] = toUpper(value);
       result[pluralize(key)] = pluralize(value);
-      result[pluralize(capitalize(key))] = pluralize(capitalize(value));
+      result[pluralize(upperFirst(key))] = pluralize(upperFirst(value));
       result[pluralize(toUpper(key))] = pluralize(toUpper(value));
     } else {
       result[key] = value;
